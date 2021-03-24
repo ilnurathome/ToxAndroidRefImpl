68209b3a2499e8d615376c17cd9988dd94f80484
Ilnur Kiamov <ilnurathome@gmail.com>
2021-03-24 11:03:25 +0300

    add select all

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/ToolbarActionMode.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/ToolbarActionMode.java
@@ -39,6 +39,8 @@ import static com.zoffcc.applications.trifa.MainActivity.selected_messages_incom
 import static com.zoffcc.applications.trifa.MainActivity.selected_messages_text_only;
 import static com.zoffcc.applications.trifa.MessageListActivity.amode;
 import static com.zoffcc.applications.trifa.MessageListActivity.amode_save_menu_item;
+import static com.zoffcc.applications.trifa.TRIFAGlobals.TRIFA_MSG_TYPE.TRIFA_MSG_FILE;
+import static com.zoffcc.applications.trifa.TRIFAGlobals.TRIFA_MSG_TYPE.TRIFA_MSG_TYPE_TEXT;
 
 public class ToolbarActionMode implements ActionMode.Callback
 {
@@ -86,49 +88,81 @@ public class ToolbarActionMode implements ActionMode.Callback
     @Override
     public boolean onActionItemClicked(ActionMode mode, MenuItem item)
     {
-        switch (item.getItemId())
-        {
-            case R.id.action_delete:
-                action_active = true;
-                // Toast.makeText(context, "You selected Delete menu.", Toast.LENGTH_SHORT).show(); // Show toast
-                if ((selected_conference_messages.isEmpty()) && (MainActivity.conference_message_list_activity == null))
-                {
-                    // normal chat view
-                    delete_selected_messages(context, true, false, "deleting Messages ...");
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_delete) {
+            action_active = true;
+            // Toast.makeText(context, "You selected Delete menu.", Toast.LENGTH_SHORT).show(); // Show toast
+            if ((selected_conference_messages.isEmpty()) && (MyMainActivity.conference_message_list_activity == null)) {
+                // normal chat view
+                delete_selected_messages(context, true, false, "deleting Messages ...");
+            } else {
+                // conference view
+                delete_selected_conference_messages(context, true, "deleting Messages ...");
+                // TODO: write me
+            }
+            mode.finish(); // Finish action mode
+        } else if (itemId == R.id.action_copy) {
+            action_active = true;
+            // Toast.makeText(context, "You selected Copy menu.", Toast.LENGTH_SHORT).show(); // Show toast
+            if ((selected_conference_messages.isEmpty()) && (MyMainActivity.conference_message_list_activity == null)) {
+                // normal chat view
+                copy_selected_messages(context);
+            } else {
+                // conference view
+                copy_selected_conference_messages(context);
+            }
+            mode.finish(); // Finish action mode
+        } else if (itemId == R.id.action_save) {
+            action_active = true;
+            // Toast.makeText(context, "You selected Copy menu.", Toast.LENGTH_SHORT).show(); // Show toast
+            save_selected_messages(context);
+            mode.finish(); // Finish action mode
+        } else if (itemId == R.id.action_select_all) {
+            if ((selected_conference_messages.isEmpty()) && (MyMainActivity.conference_message_list_activity == null)) {
+                // normal chat view
+                for (final Message message_:
+                     MyMainActivity.message_list_fragment.data_values) {
+                    if(!selected_messages.contains(message_.id)) {
+                        selected_messages.add(message_.id);
+                        if (message_.TRIFA_MESSAGE_TYPE == TRIFA_MSG_TYPE_TEXT.value) {
+                            selected_messages_text_only.add(message_.id);
+                        } else if (message_.TRIFA_MESSAGE_TYPE == TRIFA_MSG_FILE.value) {
+                            if (message_.direction == 0) {
+                                selected_messages_incoming_file.add(message_.id);
+                            }
+                        }
+                    }
                 }
-                else
-                {
-                    // conference view
-                    delete_selected_conference_messages(context, true, "deleting Messages ...");
-                    // TODO: write me
-                }
-                mode.finish(); // Finish action mode
-                break;
 
-            case R.id.action_copy:
-                action_active = true;
-                // Toast.makeText(context, "You selected Copy menu.", Toast.LENGTH_SHORT).show(); // Show toast
-                if ((selected_conference_messages.isEmpty()) && (MainActivity.conference_message_list_activity == null))
-                {
-                    // normal chat view
-                    copy_selected_messages(context);
-                }
-                else
-                {
-                    // conference view
-                    copy_selected_conference_messages(context);
-                }
-                mode.finish(); // Finish action mode
-                break;
+            } else {
+                // conference view
 
-            case R.id.action_save:
-                action_active = true;
-                // Toast.makeText(context, "You selected Copy menu.", Toast.LENGTH_SHORT).show(); // Show toast
-                save_selected_messages(context);
-                mode.finish(); // Finish action mode
-                break;
+                // TODO: write me
+            }
+
+            refreshFragment();
         }
         return false;
+    }
+
+    void refreshFragment() {
+        try {
+            if (MyMainActivity.conference_message_list_fragment != null) {
+                // need to redraw all items again here, to remove the selections
+                MyMainActivity.conference_message_list_fragment.adapter.redraw_all_items();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (MyMainActivity.message_list_fragment != null) {
+                // need to redraw all items again here, to remove the selections
+                MyMainActivity.message_list_fragment.adapter.redraw_all_items();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -144,31 +178,7 @@ public class ToolbarActionMode implements ActionMode.Callback
                 selected_messages_incoming_file.clear();
                 selected_messages_text_only.clear();
 
-                try
-                {
-                    if (MainActivity.conference_message_list_fragment != null)
-                    {
-                        // need to redraw all items again here, to remove the selections
-                        MainActivity.conference_message_list_fragment.adapter.redraw_all_items();
-                    }
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-
-                try
-                {
-                    if (MainActivity.message_list_fragment != null)
-                    {
-                        // need to redraw all items again here, to remove the selections
-                        MainActivity.message_list_fragment.adapter.redraw_all_items();
-                    }
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
+                refreshFragment();
             }
 
             if (amode != null)
